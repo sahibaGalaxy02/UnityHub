1256c0ecbf0e1d091afaa22c92ce06e4defef53d
Sahiba.J <sabaaa.s747@gmail.com>
2026-06-04 10:49:49 +0530

    Remove duplicate import of 'io' in Chat.jsx
    Removed duplicate import of 'io' from 'socket.io-client'.

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-import { io } from "socket.io-client";
-
 const socket = io("https://unityhub-wycf.onrender.com");
 
 export default function Chat() {
